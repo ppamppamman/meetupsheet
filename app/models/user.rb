4d5bd69f13e94757cc9c507d2235736bd13e5f66
Jaeuk-YOO <ryou9305@naver.com>
2019-12-17 02:00:24 +0900

    user group match by entry model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :schedules
-  belongs_to :group
+  has_many :entries
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_200259) do
+ActiveRecord::Schema.define(version: 2019_12_16_165728) do
+
+  create_table "entries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_entries_on_group_id"
+    t.index ["user_id"], name: "index_entries_on_user_id"
+  end
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
