9115f7daf9879f2c9e283c6636e69fd253cdab17
Jaeuk-YOO <ryou9305@naver.com>
2019-12-17 01:44:12 +0900

    group controller create

---
+++ app/controllers/group_controller.rb
@@ -0,0 +1,23 @@
+class GroupController < ApplicationController
+
+  def list
+  end
+
+  def find
+  end
+
+  def join
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
